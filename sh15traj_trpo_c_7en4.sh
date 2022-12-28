61fbf4a0a87c9271237640b1c78fa88134ae8b06
sry <ruoyu.sun@manchester.ac.uk>
2022-12-28 18:02:00 +0000

    config

--- sh15traj_trpo_c_7en4.sh
+++ sh15traj_trpo_c_7en4.sh
@@ -78,7 +78,7 @@ done
 
 # ===================================
 cd ~/RL-frenet-trajectory-planning-in-CARLA/
-python3 ./run_BDPL.py -p $port --tm_port $port_tm --cfg_file=tools/cfgs_10vehicles/config_trpo_15traj.yaml --agent_id=5579 --env=CarlaGymEnv-v5 --learning_rate=7e-4 --planner_mode=catagorical --numtraj=15
+python3 ./run_BDPL.py -p $port --tm_port $port_tm --cfg_file=tools/cfgs_10vehicles/config_trpo_15traj.yaml --agent_id=5578 --env=CarlaGymEnv-v5 --learning_rate=7e-4 --planner_mode=catagorical --num_traj=15
 
 
 #now close the server pid
